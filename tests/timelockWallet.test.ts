959d4ad15b956943dbac41ff4913cc1d075752d4
Alao <alaomuideenabiola@gmail.com>
2023-12-31 13:50:29 -0800

    commit 2

--- tests/timelockWallet.test.ts
+++ tests/timelockWallet.test.ts
@@ -7,13 +7,13 @@ import { describe } from 'mocha'
 import { myPublicKey } from './utils/privateKey'
 use(chaiAsPromised)
 
-describe('Test SmartContract `Timelock`', () => {
+describe('Test SmartContract `Timelockwallet`', () => {
     let instance: TimelockWallet
     const [, , , fatherAddr] = randomPrivateKey()
     const [, , , motherAddr] = randomPrivateKey()
     const [, , , brotherddr] = randomPrivateKey()
     const [, , , sisterAddr] = randomPrivateKey()
-    const lockTimeMin = Math.round(new Date('2022-12-30').valueOf() / 1000)
+    const lockTimeMin = Math.round(new Date('2023-12-30').valueOf() / 1000)
     before(async () => {
         await TimelockWallet.loadArtifact()
 
@@ -43,7 +43,7 @@ describe('Test SmartContract `Timelock`', () => {
                     lockTime: today,
                 } as MethodCallOptions<TimelockWallet>
             )
-            console.log(`Called "unlock" method: ${callRes.tx.id}`)
+            console.log(`Called TXID: ${callRes.tx.id}`)
         }
         await expect(call()).not.to.be.rejected
     })
